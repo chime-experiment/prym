41ba122f56321f456f4e2b800a9a4aa038a35539
anjakefala <anja.kefala@gmail.com>
2020-10-20 16:25:36 -0700

    build: initial commit of build tools

---
+++ setup.py
@@ -0,0 +1,23 @@
+from os import path
+from setuptools import setup, find_packages
+
+import versioneer
+
+here = path.abspath(path.dirname(__file__))
+
+# load the requirements from requirements.txt while removing the environment marks
+with open(path.join(here, "requirements.txt")) as f:
+    requirements = [line.split(";")[0].rstrip() for line in f]
+
+setup(
+        name="prym",
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        description="Convert results of PromQL queries into numpy arrays or pandas dataframes",
+        url="https://github.com/chime-experiment/prym",
+        author="Richard Shaw",
+        python_requires=">=3.6",
+        py_modules=["prym"],
+        install_requires=requirements,
+        packages=find_packages(),
+)
